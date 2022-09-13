a64f994b9e0110be2e80ae29a7f282e4c262d3dc
MickeyDaMouse <72903391+MickeyDaMouse@users.noreply.github.com>
2022-09-13 12:37:05 -0700

    Add files via upload

---
+++ review.java
@@ -0,0 +1,10 @@
+public class review {
+    public static void main (String[] args)
+    {
+
+        System.out.println("Hello World");
+        System.out.println("Hello, I am Mickey's Laptop");
+        System.out.println("He took this course because he is very interested in programming." + "Also, he would like to review about programming!");
+
+    }
+}
